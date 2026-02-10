66f05967c17b31a78cc87dfc1a00532448964d93
fecepedac-creator <fecepedac@gmail.com>
2026-02-10 17:01:26 -0300

    Update vite.config.ts
    Aplicable a firebase

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,17 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(() => {
-    return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
+  return {
+    server: {
+      host: true,
+      port: Number(process.env.PORT) || 8080,
+    },
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '.'),
       },
-      plugins: [react()],
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
+    },
+  };
 });
+
